32a172e36480469843ad766670c4c1f686eebd77
Roman <romantut1988@mail.ru>
2023-04-05 19:22:59 +0300

    eighth task

---
+++ index_8.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index_8</title>
+</head>
+
+<body>
+    {
+    "object": {
+    "compare": "=",
+    "value": "Квартиры"
+    },
+    "type" : {
+    "compare": "=",
+    "value": "Продажа"
+    },
+    "status" : {
+    "compare": "=",
+    "value": "Новостройка"
+    }
+    }
+</body>
+
+</html>
